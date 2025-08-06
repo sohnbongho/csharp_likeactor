9c73f80a14eb1995c6595727c3953c317356cc66
bhsohn <hanmuson1@gmail.com>
2025-08-06 15:05:24 +0900

    keep alive 관련 처리

--- DummyClient/Session/Handler/Remote/KeepAliveNotiHandler.cs
+++ DummyClient/Session/Handler/Remote/KeepAliveNotiHandler.cs
@@ -4,16 +4,16 @@ using Library.MessageQueue.Attributes.Remote;
 using Library.MessageQueue.Message;
 using Messages;
 
-namespace DummyClient.Session.Handler;
+namespace DummyClient.Session.Handler.Remote;
 
-[RemoteMessageHandlerAsyncAttribute(MessageWrapper.PayloadOneofCase.KeepAliveNoti)]
+[RemoteMessageHandlerAsync(MessageWrapper.PayloadOneofCase.KeepAliveNoti)]
 public class KeepAliveNotiHandler : IRemoteMessageHandlerAsync
 {
     private readonly IServerLogger _logger = ServerLoggerFactory.CreateLogger();
     public async Task<bool> HandleAsync(IMessageQueueReceiver receiver, MessageWrapper message)
     {
         _logger.Debug(() => $"[SessionId:{receiver.SessionId}]KeepAliveRequestHandler");
-        
+
         await Task.Delay(1000);
 
         {
